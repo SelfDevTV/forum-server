956b4bca2bfddd2c1767c2e47a0859dbe952b100
Dominik Huber <dhuber666@gmail.com>
2019-06-04 18:58:05 +0200

    added hooks with error :(

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,10 @@
 import { Button, Container } from "@material-ui/core";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Home = props => {
+  const [test, setTest] = useState("");
+
   return (
     <Container>
       <Link href="/api/auth/facebook">
