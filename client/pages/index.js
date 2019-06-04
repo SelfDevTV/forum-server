21ad130b7db3bdf7d3a1cd5c97e9d1b3b8417cc9
Huber Dominik <Huberdo@voestalpine.root.local>
2019-06-04 15:46:42 +0200

    added material ui react

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-const Home = () => <h2>Hello world43</h2>;
+import { Button, Container } from "@material-ui/core";
+import Link from "next/link";
+
+const Home = props => {
+  return (
+    <Container>
+      <Link href="/api/auth/facebook">
+        <Button variant="contained" color="secondary">
+          Login
+        </Button>
+      </Link>
+    </Container>
+  );
+};
 
 export default Home;
